f2eddaf29a70fe338a8c1e22a387e7ccc0df9d52
Hastur5 <mgasca12@gmail.com>
2023-06-07 23:56:23 -0600

    feat: se crea carpeta Middleware

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,8 +1,17 @@
 const getProducts = (req, res) => {
   res.status(200).json({ mensaje: "Mostrar las tareas" });
+  console.log(req.body);
 };
 const setProducts = (req, res) => {
-  res.status(200).json({ mensaje: "set" });
+  console.log(req.body);
+  if (!req.body.nombre) {
+    res
+      .status(400)
+      .json({ mensaje: "Favor de teclear la descripcion del producto" });
+    res.status;
+    res.status(201).json({ mensaje: "Crear un Producto" })(400);
+    throw new Error("Favor de teclear la descripcion del producto ");
+  }
 };
 const updateProducts = (req, res) => {
   res.status(200).json({ mensaje: "up" });
@@ -15,5 +24,5 @@ module.exports = {
   getProducts,
   setProducts,
   updateProducts,
-  deleteProducts
+  deleteProducts,
 };
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/productos", require("./routes/products.routes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server iniciando en el puerto ${port}`));
 
